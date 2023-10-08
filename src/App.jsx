11c8d066a194cc528cd50f1f86f61cdc77b81f30
AbdelhamidYousef <abdelhamedy33@gmail.com>
2023-10-08 23:55:24 +0300

    Sidebar Toggle Button - Fixes & Modifications

--- src/App.jsx
+++ src/App.jsx
@@ -186,13 +186,11 @@ const App = () => {
   return (
     <div className="flex">
       {/* Sidebar Toggle Button */}
-      {/* To fix the bug of disappearing in small window when the sidebar is on */}
-      {/* To change the elements to be consistent with top button */}
-      <div className="lg:hidden fixed left-5 top-5 w-12 h-12 rounded-full bg-black/30 z-10 grid place-content-center focus-within:border-2 focus-within:border-black/20">
-        <button
-          className="w-5 h-5 flex items-center focus:outline-none"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        >
+      <button
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        className="lg:hidden fixed left-5 top-5 w-12 h-12 rounded-full bg-slate-700/60 z-20 grid place-content-center hover:bg-slate-700/50 focus:outline-none focus:ring-2 focus:ring-slate-700/60 focus:ring-offset-2 transition-all duration-300"
+      >
+        <span className="w-5 h-5 flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 448 512"
@@ -210,14 +208,12 @@ const App = () => {
           >
             <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
           </svg>
-        </button>
-      </div>
+        </span>
+      </button>
 
       {/* To Top Button */}
       <button
-        onClick={() =>
-          setTimeout(() => window.scrollTo({ top: 0, behavior: "smooth" }), 10)
-        }
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         className="fixed right-5 bottom-5 sm:right-10 sm:bottom-6 w-10 h-10 sm:w-12 sm:h-12 rounded-full bg-primary-600 z-10 grid place-content-center hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 transition-all duration-300"
       >
         <span className="w-5 h-5 sm:w-6 sm:h-6 ">
