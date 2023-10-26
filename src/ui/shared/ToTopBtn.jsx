cb755e66fb8248eb49ce101219442ace66afc6a4
Abdulhamid <abdelhamedy33@gmail.com>
2023-10-26 15:33:22 +0300

    made to-top-button hidden at top & bottom

--- src/ui/shared/ToTopBtn.jsx
+++ src/ui/shared/ToTopBtn.jsx
@@ -1,10 +1,17 @@
 import UpArrow from "../svgs/UpArrow";
+import { useActiveSection } from "../../context/ActiveSection";
 
 const ToTopBtn = () => {
+  const { activeSection } = useActiveSection();
+  const hidden =
+    !activeSection || activeSection === "about" || activeSection === "contact";
+
   return (
     <button
       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-      className="fixed right-5 bottom-5 sm:right-10 sm:bottom-6 w-10 h-10 sm:w-12 sm:h-12 rounded-full bg-primary-600 z-10 grid place-content-center hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 transition-all duration-300"
+      className={`${
+        hidden ? "opacity-0 invisible" : "opacity-100 visible"
+      } fixed right-5 bottom-5 sm:right-10 sm:bottom-6 w-10 h-10 sm:w-12 sm:h-12 rounded-full bg-primary-600 z-10 grid place-content-center hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 transition-all duration-300`}
     >
       <span className="w-5 h-5 sm:w-6 sm:h-6 text-white">
         <UpArrow />
