82c1bc4c25f6e7fa8e41c9cf51b09f957e09b6b0
Ubuntu2 <abdelhamedy33@gmail.com>
2025-12-17 00:53:17 +0200

    Skills

--- src/ui/layouts/skills/Skills.jsx
+++ src/ui/layouts/skills/Skills.jsx
@@ -1,9 +1,7 @@
 import PropTypes from 'prop-types';
-import SectionTitle from '../../shared/SectionTitle';
-import SkillsContainer from './SkillsContainer';
 import Section from '../../shared/Section';
 import { ListProvider } from '../../../library/context/ListContext';
-import SkillsOperations from './SkillsOperations';
+import SkillsContent from './SkillsContent';
 
 const Skills = ({ content }) => {
   const { skillsList, skillsFilters, skillsSearch } = content;
@@ -15,16 +13,10 @@ const Skills = ({ content }) => {
         filterKey={skillsFilters.key}
         searchKey={skillsSearch.key}
       >
-        <div className="mb-10 md:mb-20 flex flex-col items-center gap-y-10 md:flex-row md:justify-between md:items-center">
-          <SectionTitle className="!m-0">Skills</SectionTitle>
-
-          <SkillsOperations
-            skillsFilters={skillsFilters}
-            skillsSearch={skillsSearch}
-          />
-        </div>
-
-        <SkillsContainer list={skillsList} />
+        <SkillsContent
+          skillsFilters={skillsFilters}
+          skillsSearch={skillsSearch}
+        />
       </ListProvider>
     </Section>
   );
---
+++ src/ui/layouts/skills/SkillsContent.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import SectionTitle from '../../shared/SectionTitle';
+import IconCardList from '../../../library/components/general/icon-card-list/IconCardList';
+import { useList } from '../../../library/context/ListContext';
+import SkillsOperations from './SkillsOperations';
+
+const SkillsContent = ({ skillsFilters, skillsSearch }) => {
+  const { list } = useList();
+
+  return (
+    <>
+      <div className="mb-10 md:mb-20 flex flex-col items-center gap-y-10 md:flex-row md:justify-between md:items-center">
+        <SectionTitle className="!m-0">Skills</SectionTitle>
+
+        <SkillsOperations
+          skillsFilters={skillsFilters}
+          skillsSearch={skillsSearch}
+        />
+      </div>
+
+      <IconCardList
+        list={list}
+        className="min-h-[61rem] sm:min-h-[34rem] 2xl:min-h-[30rem]"
+      />
+    </>
+  );
+};
+
+SkillsContent.propTypes = {
+  skillsFilters: PropTypes.shape({
+    placeholder: PropTypes.string.isRequired,
+    key: PropTypes.string.isRequired,
+    options: PropTypes.array.isRequired,
+  }).isRequired,
+  skillsSearch: PropTypes.shape({
+    placeholder: PropTypes.string.isRequired,
+    key: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default SkillsContent;
