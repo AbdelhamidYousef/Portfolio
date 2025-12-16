a6a79e40503bbb1549e3301b4a26764a2314b5e2
Ubuntu2 <abdelhamedy33@gmail.com>
2025-12-16 21:50:43 +0200

    Projects

--- src/library/components/technologies/Technologies.jsx
+++ src/library/components/technologies/Technologies.jsx
@@ -1,10 +1,34 @@
 import PropTypes from 'prop-types';
 import Icon, { hasIcon } from '../icon/Icon';
 
-const Technologies = ({ content, className = '', itemClassName = '' }) => {
+/**
+ * Technologies component that renders a list of technologies
+ *
+ * @param {Object} props - The component props
+ * @param {Array} props.content - The content of the technologies
+ * @param {string} props.containerClassName - Optional containerClassName to pass to the container
+ * @param {string} props.itemClassName - Optional containerClassName to pass to the item
+ * @param {string} props.iconClassName - Optional containerClassName to pass to the icon
+ *
+ * @returns {JSX.Element} The technologies component
+ *
+ * @example
+ * <Technologies
+ *   content={technologies}
+ *   containerClassName="mt-4 xs:mt-5 gap-x-4 justify-center xs:justify-center"
+ *   itemClassName="[&>svg]:w-8 [&>svg]:h-8 xs:[&>svg]:w-9 xs:[&>svg]:h-9 drop-shadow-[0_0_8px_rgba(255,255,255,0.2)]"
+ *   iconClassName="w-8 h-8"
+ * />
+ */
+const Technologies = ({
+  content,
+  containerClassName = '',
+  itemClassName = '',
+  iconClassName = '',
+}) => {
   return (
     <ul
-      className={`xs:ml-1 flex flex-wrap justify-center xs:justify-start gap-x-4 sm:gap-x-5 md:gap-x-6 gap-y-3 ${className}`}
+      className={`xs:ml-1 flex flex-wrap justify-center xs:justify-start gap-x-4 sm:gap-x-5 md:gap-x-6 gap-y-3 ${containerClassName}`}
     >
       {content.map((tech) => {
         if (!hasIcon(tech.iconName)) return null;
@@ -15,7 +39,7 @@ const Technologies = ({ content, className = '', itemClassName = '' }) => {
             className={`grid place-content-center drop-shadow-[0_0_3px_#33333340] cursor-help hover:scale-110 sm:hover:scale-[1.2] transition-all duration-300 [&>svg]:w-6 [&>svg]:h-6 xs:[&>svg]:w-8 xs:[&>svg]:h-8 lg:[&>svg]:w-9 lg:[&>svg]:h-9 ${itemClassName}`}
             data-title-bottom={tech.title}
           >
-            <Icon name={tech.iconName} />
+            <Icon name={tech.iconName} className={iconClassName} />
           </li>
         );
       })}
@@ -25,8 +49,9 @@ const Technologies = ({ content, className = '', itemClassName = '' }) => {
 
 Technologies.propTypes = {
   content: PropTypes.array.isRequired,
-  className: PropTypes.string,
+  containerClassName: PropTypes.string,
   itemClassName: PropTypes.string,
+  iconClassName: PropTypes.string,
 };
 
 export default Technologies;
