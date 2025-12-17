bd6cabfc80270cfe02b304355efeada82aaa5e65
Ubuntu2 <abdelhamedy33@gmail.com>
2025-12-17 02:33:10 +0200

    Skills

--- src/library/components/specific/filter-and-search/Searchbar.jsx
+++ src/library/components/specific/filter-and-search/Searchbar.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import SearchIcon from '../../../svgs/SearchIcon';
+import CloseIcon from '../../../svgs/CloseIcon';
 import { useKeydown } from '../../../hooks/useKeydown';
 
 /**
@@ -40,14 +41,7 @@ const Searchbar = ({
           className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 flex items-center justify-center text-gray-400 hover:text-gray-600 dark:hover:text-gray-200 transition-colors duration-200"
           aria-label="Clear search"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-4 h-4"
-          >
-            <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-          </svg>
+          <CloseIcon className="w-4 h-4" />
         </button>
       )}
     </div>
