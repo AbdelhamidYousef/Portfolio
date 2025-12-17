b9ed23222b35c75e0a86f8e197e7e242c5c5d2d3
Ubuntu2 <abdelhamedy33@gmail.com>
2025-12-17 02:26:40 +0200

    Skills

--- src/library/components/general/cards/icon-card-list/IconCardList.jsx
+++ src/library/components/general/cards/icon-card-list/IconCardList.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { m } from 'framer-motion';
+import { m, AnimatePresence } from 'framer-motion';
 import { fadeIn } from '../../../../../utils/motion';
 import Icon, { hasIcon } from '../../Icon';
 
@@ -28,32 +28,39 @@ const IconCardList = ({
     <ul
       className={`grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 content-start gap-3 sm:gap-4 lg:gap-5 ${containerClassName}`}
     >
-      {list.map((item, index) => {
-        if (!hasIcon(item.iconName)) {
-          console.error(`IconCardList: Icon for [${item.iconName}] not found`);
-          return null;
-        }
+      <AnimatePresence>
+        {list.map((item, index) => {
+          if (!hasIcon(item.iconName)) {
+            console.error(
+              `IconCardList: Icon for [${item.iconName}] not found`
+            );
+            return null;
+          }
 
-        return (
-          <m.li
-            key={item.id}
-            variants={fadeIn('left', 'spring', 1.8, 0.2 * index)}
-          >
-            <div
-              className={`group relative h-16 xl:h-20 rounded-xl border border-gray-100 shadow-sm overflow-hidden flex items-center hover:scale-105 transition-all duration-300 dark:bg-slate-700 dark:border-gray-600 ${itemClassName}`}
+          return (
+            <m.li
+              key={item.id}
+              variants={fadeIn('left', 'spring', 1, 0.1 * index)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
             >
-              <p className="px-6 text-sm sm:text-base xl:font-medium text-slate-500 tracking-wider relative z-10 dark:text-gray-50">
-                {item.title}
-              </p>
+              <div
+                className={`group relative h-16 xl:h-20 rounded-xl border border-gray-100 shadow-sm overflow-hidden flex items-center hover:scale-105 transition-all duration-300 dark:bg-slate-700 dark:border-gray-600 ${itemClassName}`}
+              >
+                <p className="px-6 text-sm sm:text-base xl:font-medium text-slate-500 tracking-wider relative z-10 dark:text-gray-50">
+                  {item.title}
+                </p>
 
-              <span className="absolute right-0 top-1/2 -translate-y-1/2 w-1/3 group-hover:-rotate-6 transition-all duration-300">
-                <Icon name={item.iconName} />
-              </span>
-              <span className="absolute inset-0 bg-gradient-to-r from-white via-white via-60% to-white/60 dark:from-slate-700 dark:via-slate-700 dark:to-gray-slate-700/60"></span>
-            </div>
-          </m.li>
-        );
-      })}
+                <span className="absolute right-0 top-1/2 -translate-y-1/2 w-1/3 group-hover:-rotate-6 transition-all duration-300">
+                  <Icon name={item.iconName} />
+                </span>
+                <span className="absolute inset-0 bg-gradient-to-r from-white via-white via-60% to-white/60 dark:from-slate-700 dark:via-slate-700 dark:to-gray-slate-700/60"></span>
+              </div>
+            </m.li>
+          );
+        })}
+      </AnimatePresence>
     </ul>
   );
 };
