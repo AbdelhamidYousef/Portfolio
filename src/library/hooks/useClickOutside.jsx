260b9cddcd311621751d690cdf2d110826f86b2f
Ubuntu2 <abdelhamedy33@gmail.com>
2026-01-14 22:30:54 +0200

    Clean slate for v3.0 rewrite

--- src/library/hooks/useClickOutside.jsx
+++
@@ -1,8 +0,0 @@
-export const useClickOutside = (className, handler) => {
-  const callback = (e) => {
-    if (!e.target.closest(`.${className}`)) handler();
-  };
-
-  document.addEventListener('click', callback);
-  return () => document.removeEventListener('click', callback);
-};
