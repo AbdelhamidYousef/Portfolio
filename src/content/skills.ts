d364618e48f32af00e403efe89be01155a2295fe
Ubuntu2 <abdelhamedy33@gmail.com>
2026-02-17 20:57:08 +0200

    feature/at-update - Deployment fix

--- src/content/skills.ts
+++ src/content/skills.ts
@@ -14,50 +14,15 @@ export const skillCategories: { value: string; label: string }[] = [
 export const skills: Skill[] = [
   // Languages
   { id: 1, title: 'HTML5', iconName: 'html', type: 'web' },
-  {
-    id: 2,
-    title: 'CSS3 (Tailwind/SASS)',
-    iconName: 'css',
-    type: 'web',
-  },
-  {
-    id: 3,
-    title: 'JavaScript (ES6+)',
-    iconName: 'javascript',
-    type: 'web',
-  },
-  {
-    id: 3,
-    title: 'TypeScript',
-    iconName: 'typescript',
-    type: 'web',
-  },
+  { id: 2, title: 'CSS3 (Tailwind/SASS)', iconName: 'css', type: 'web' },
+  { id: 3, title: 'JavaScript (ES6+)', iconName: 'javascript', type: 'web' },
+  { id: 4, title: 'TypeScript', iconName: 'typescript', type: 'web' },
   // Frameworks
-  { id: 4, title: 'React', iconName: 'react', type: 'web' },
-  { id: 5, title: 'Magento', iconName: 'magento', type: 'web' },
+  { id: 5, title: 'React', iconName: 'react', type: 'web' },
+  { id: 6, title: 'Magento', iconName: 'magento', type: 'web' },
   // Adobe Experience Cloud
-  {
-    id: 6,
-    title: 'Adobe Target',
-    iconName: 'adobetarget',
-    type: 'aec',
-  },
-  {
-    id: 6,
-    title: 'Adobe Launch / Tags',
-    iconName: 'adobeexperiencecloud',
-    type: 'aec',
-  },
-  {
-    id: 7,
-    title: 'A4T (Analytics for Target)',
-    iconName: 'adobeanalytics',
-    type: 'aec',
-  },
-  {
-    id: 8,
-    title: 'Adobe Audience Manager',
-    iconName: 'adobeaudiencemanager',
-    type: 'aec',
-  },
+  { id: 7, title: 'Adobe Target', iconName: 'adobetarget', type: 'aec' },
+  { id: 8, title: 'Adobe Launch / Tags', iconName: 'adobeexperiencecloud', type: 'aec' },
+  { id: 9, title: 'A4T (Analytics for Target)', iconName: 'adobeanalytics', type: 'aec' },
+  { id: 10, title: 'Adobe Audience Manager', iconName: 'adobeaudiencemanager', type: 'aec' },
 ];
