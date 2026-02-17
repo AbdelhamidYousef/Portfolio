9a8f170677f48a4756374ed3e08dad0061ebfb6f
Ubuntu2 <abdelhamedy33@gmail.com>
2026-02-17 18:59:46 +0200

    feature/at-update - Updated the experiences section

--- src/content/experience.ts
+++ src/content/experience.ts
@@ -11,17 +11,19 @@ export interface WorkExperience {
 export const workExperience: WorkExperience[] = [
   {
     id: 1,
-    title: 'Full Stack Developer | React Developer',
+    title:
+      'Certified Adobe Target Engineer | Personalization & Experimentation',
     company: 'Scandiweb',
-    period: '2024–present',
+    period: '2023–present',
     location: 'Latvia - Remote',
     skills: [
-      'TypeScript',
-      'React',
-      'React Router',
-      'React Query',
-      'Tailwind CSS',
-      'SASS',
+      'Adobe Target',
+      'A4T',
+      'Adobe Tags',
+      'JavaScript',
+      'Personalization',
+      'Experimentation',
+      'A/B Testing',
     ],
     responsibilities: [
       'Transitioned from a full-stack role to focus on frontend development, specializing in React and TypeScript.',
@@ -29,24 +31,6 @@ export const workExperience: WorkExperience[] = [
       'Built responsive, accessible, and visually appealing interfaces, optimizing for speed and usability.',
       'Integrated frontend with backend services via APIs and GraphQL, ensuring seamless data flow.',
       'Wrote clean, maintainable, and well-documented code, following best practices and modern frontend architecture.',
-    ],
-  },
-  {
-    id: 2,
-    title: 'Personalization & Adobe Target Developer',
-    company: '85Sixty',
-    period: '2024–Present',
-    location: 'USA - Remote',
-    skills: ['SASS', 'JavaScript', 'Adobe Target', 'Adobe Analytics'],
-    responsibilities: [
-      'Setting up A/B, multivariate, and experience targeting activities with Adobe Target.',
-      'Setting up ML-based features of Adobe Target including Auto-Target, Automated Personalizations, and Recommendations.',
-      'Managing static and dynamic (React-based) websites with vanilla JavaScript custom code from within Target.',
-      'Implementing audience-based targeting strategies.',
-      'Managing tracking using traditional at.js and more advanced WebSDK.',
-      'Creating and managing different treatments to enhance user interactions.',
-      'Ensuring seamless delivery of personalized content based on data-driven decisions.',
-      'Developing and updating a utility library to automate the generation of the code for different activities.',
     ],
   },
 ];
